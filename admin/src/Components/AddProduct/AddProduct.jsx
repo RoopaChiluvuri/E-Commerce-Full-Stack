1133a0d5b56befaddad05f5684dab25c50aeda9a
RoopaChiluvuri <roopasree2509@gmail.com>
2024-07-02 20:16:08 -0700

    added uploadImage functionality

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -19,6 +19,35 @@ const AddProduct = () => {
     const changeHandler = (e) =>{
         setProductDetails({...productDetails,[e.target.name]:e.target.value})
     }
+    const Add_Product = async ()=>{
+        console.log(productDetails);
+        let responseData;
+        let product = productDetails;
+
+        let formData = new FormData();
+        formData.append('product',image);
+
+        await fetch('http://localhost:4000/upload',{
+            method:'POST',
+            headers:{
+                Accept: 'application/json',
+            },
+            body:formData,
+            
+        }).then((resp) =>resp.json()).then((data)=>{responseData=data});
+
+        if(responseData.success)
+        {
+            product.image = responseData.image_url;
+            console.log(product);
+            await fetch('http://localhost:4000/addproduct',{
+                method:'POST',
+                headers:{
+                    Accept:'application/json'
+                }
+            })
+        }
+    }
 
   return (
     <div className='add-product'>
@@ -50,7 +79,7 @@ const AddProduct = () => {
             </label>
             <input onChange={imageHandler} type="file" name='image' id="file-input" hidden/>
         </div>
-        <button className='addproduct-btn'>ADD</button>
+        <button onClick={()=>{Add_Product()}} className='addproduct-btn'>ADD</button>
     </div>
   )
 }
